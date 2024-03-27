41e9a80244108b0516475367aa78760fe933ebd5
LeadRDRK <leadrdrk@zohomail.com>
2024-03-27 22:33:22 +0700

    Simplify index structure

--- gen-index.js
+++ gen-index.js
@@ -1,33 +1,22 @@
 const fs = require("fs");
 const { blake3 } = require("hash-wasm");
 
-async function genDirTree(dir, currentArr) {
+async function walkDir(dir, callback) {
     let files = fs.readdirSync(dir);
     for (const name of files) {
         let path = dir + "/" + name;
-        console.log(path);
         let stats = fs.statSync(path);
-        let entry;
         if (stats.isDirectory()) {
-            entry = {
-                type: "Directory",
-                name,
-                nodes: []
-            };
-            await genDirTree(path, entry.nodes);
+            await callback({path, stats});
+            await walkDir(path, callback);
         }
         else {
-            entry = {
-                type: "File",
-                name,
-                hash: await blake3(fs.readFileSync(path)),
-                size: stats.size
-            }
+            await callback({path, stats});
         }
-        currentArr.push(entry);
     }
-}
+};
 
+const LOCALIZED_DATA_DIR = "localized_data";
 async function main() {
     let index = JSON.parse(fs.readFileSync("index_base.json", "utf8"));
     if (typeof index != "object") {
@@ -35,8 +24,19 @@ async function main() {
         process.exit(1);
     }
 
-    index.dir_tree = [];
-    await genDirTree("localized_data", index.dir_tree);
+    let files = [];
+    await walkDir(LOCALIZED_DATA_DIR, async entry => {
+        let { path, stats } = entry;
+        if (stats.isDirectory()) return;
+        let relPath = path.slice(LOCALIZED_DATA_DIR.length + 1);
+        console.log(relPath);
+        files.push({
+            path: relPath,
+            hash: await blake3(fs.readFileSync(path)),
+            size: stats.size
+        })
+    });
+    index.files = files;
 
     fs.writeFileSync("index.json", JSON.stringify(index, null, 2), "utf8");
 }
